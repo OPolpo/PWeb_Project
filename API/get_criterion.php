b243f67213ce86355e7a516a2f338422c36a4502
LaCele <m.celeste.grandi@gmail.com>
2014-07-06 15:16:25 +0200

    forgetfulness...

--- API/get_criterion.php
+++ API/get_criterion.php
@@ -9,9 +9,9 @@ require_once 'config.php';
 function get_criterion(){
  	global $con;
  	mysqli_set_charset($con, 'utf8');
- 	$sql_get_property="SELECT coefficient_name as name, id_criterion as id FROM criterion;";
+ 	$sql_get_criterion="SELECT coefficient_name as name, id_criterion as id FROM criterion;";
  	$criterion_list = array();
- 	$result = mysqli_query($con, $sql_get_property);
+ 	$result = mysqli_query($con, $sql_get_criterion);
  	while ($obj = mysqli_fetch_object($result))
  		$criterion_list[] = $obj;
  	return json_encode($criterion_list);
